714bf844a0669080d8d94919f54db5f8bfa720ee
Md Mojibul Rahaman <mdmojibulr434@gmail.com>
2025-08-26 00:43:09 +0545

    RemoveBtn.jsx file has been is updated

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,6 +1,34 @@
-import { HiOutlineTrash } from "react-icons/hi"
-export default function RemoveBtn(){
-    return <button className="text-red-400">
-        <HiOutlineTrash size={24}/>
+"use client";
+
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
+
+export default function RemoveBtn({ id }) {
+  const router = useRouter();
+
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure you want to delete this student?");
+    
+    if (confirmed) {
+      try {
+        const res = await fetch(`/api/topics?id=${id}`, {
+          method: "DELETE",
+        });
+
+        if (res.ok) {
+          router.refresh();
+        } else {
+          throw new Error("Failed to delete student");
+        }
+      } catch (error) {
+        console.log("Error deleting student:", error);
+      }
+    }
+  };
+
+  return (
+    <button onClick={removeTopic} className="text-red-400 hover:text-red-600">
+      <HiOutlineTrash size={24} />
     </button>
+  );
 }

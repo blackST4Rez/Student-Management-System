ad4f5089af287e4fcc9a37ecc021b86d6c2a6df8
blackST4Rez <maharjanraka2015@gmail.com>
2025-08-25 15:04:29 +0545

    Add Student page is completed

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,20 @@
+export default function AddTopic() {
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Student Name"
+      />
+
+       <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Course"
+      />
+      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+        Update Database
+      </button>
+    </form>
+  );
+}

353889a02228b0c26ecbafdaf2dbbd4bebf5ce11
blackST4Rez <maharjanraka2015@gmail.com>
2025-08-25 14:48:21 +0545

    Udpate and delete buttons are added

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,4 @@
 import TopicsList from "@/components/TopicsList";
-import Image from "next/image";
-
 export default function Home() {
   return <TopicsList/>;
 }
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
+import {HiPencilAlt} from "react-icons/hi"
+
 export default function TopicsList() {
-  return <h1>Topics</h1>;
+  return (
+    <>
+    <div>
+        <div>
+            <h2>Student Name</h2>
+            <div>Course</div>
+        </div>
+
+        <div>
+            <RemoveBtn />
+            <Link href={'/editTopic/123'}>
+            <HiPencilAlt size={24} />
+            </Link>
+        </div>
+    </div>
+    </>
+  );
 }

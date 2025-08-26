d0a4a4fdfe0a24d22d687beea0a059598ee71382
sodeep5 <kathariyasudip94@gmail.com>
2025-08-26 12:11:36 +0545

    Add student UI  is updated

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,18 +1,36 @@
+"use client";
+
 export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+
+    if(!title || !description){
+      alert("Title and description are required.");
+    }
+
+  };
+
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
       <input
+      onChange={(e)=> setTitle(e.target.value)}
+      value={title}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Student Name"
       />
 
        <input
+        onChange={(e)=> setDescription(e.target.value)}
+      value={title}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Course"
       />
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+      <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
         Add Student
       </button>
     </form>
